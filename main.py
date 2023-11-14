3cd170b96d82aa32f4cc962c9ff6d6a92cdd8d21
Evgeny Safonov <jeka-luda@yandex.ru>
2023-11-14 16:40:31 +0300

    add functions get_currency_rate & save_to_json

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
+from datetime import datetime
+import os
 import requests
+import json
 
-url = "https://apilayer.com/marketplace/exchangerates_data-api?access_key=2130b8f33395570b554d0e51114c86e4&base=EUR"
+API_KEY = 'xcbDQ9fSwl2Z9OkMwe44Yx5CUCdBd5qB'
+CURRENCY_RATES_FILE = 'currency_rates.json'
+# API_KEY = os.getenv('RATE_API_KEY')
 
-payload = {}
-headers = {
-    "apikey": "2130b8f33395570b554d0e51114c86e4"
-}
+def main():
+    while True:
+        currency = input("Введите название валюты (USD или EUR): ")
+        if currency not in ("USD", "EUR"):
+            print("Некорректный ввод")
+            continue
+        rate = get_currency_rate(currency)
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-response = requests.request("GET", url, headers=headers, data=payload)
-result_json = response.json()
+        print(f'Курс {currency} к рублю: {rate}')
+        data = {'currency': currency, 'rate': rate, 'timestamp': timestamp}
+        save_to_json(data)
 
-print(result_json)
+        choice = input('Выберите действие: (1 - продолжить, 2 - выйти) ')
+        if choice == '1':
+            continue
+        elif choice == '2':
+            break
+        else:
+            print('Некорректный ввод')
+
+
+def get_currency_rate(base: str) -> float:
+    """Получает курс от API и возвращает его в виде float"""
+    url = "https://api.apilayer.com/exchangerates_data/latest"
+
+    response = requests.get(url, headers={'apikey': API_KEY}, params={'base': base})
+    rate = response.json()['rates']['RUB']
+    return rate
+
+
+def save_to_json(data: dict) -> None:
+    """Сохраняет данные в json файл."""
+    with open(CURRENCY_RATES_FILE, 'a') as f:
+        if os.stat(CURRENCY_RATES_FILE).st_size == 0:
+            json.dump([data], f)
+        else:
+            with open(CURRENCY_RATES_FILE) as f:
+                data_list = json.load(f)
+                data_list.append(data)
+            with open(CURRENCY_RATES_FILE, 'w') as f:
+                json.dump(data_list, f)
+
+
+if __name__ == "__main__":
+    main()
